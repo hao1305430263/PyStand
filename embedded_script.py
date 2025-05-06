9b34d083540e1a3b2ebe84012dbbcc93a0750b24
兔子园的大萝卜 <hao1305430263@gmail.com>
2025-05-06 10:30:25 +0800

    替换测试文件中的中文为英文

--- embedded_script.py
+++ embedded_script.py
@@ -1,10 +1,9 @@
-# 这是将会被嵌入到PyStand.exe中的Python脚本
 import os
 import sys
 
 
 def main():
-    msg = "这是从编译时嵌入的Python脚本运行的!"
+    msg = "hollo world!"
     print(msg)
     print()
 
@@ -12,7 +11,7 @@ def main():
     for path in sys.path:
         print(">", path)
 
-    os.MessageBox(msg, "嵌入式脚本")
+    os.MessageBox(msg, "hi")
 
 
 if __name__ == "__main__":
